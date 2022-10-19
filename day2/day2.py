f9c475e0c9ce2e9f6b638f57969109db1bdbdcae
NinjaArmy <jm@bitsandlikes.de>
2022-10-19 17:45:36 +0200

    refactoring and solving part 2 of task 2

--- day2/day2.py
+++ day2/day2.py
@@ -2,63 +2,28 @@
 import re
 
 file = open('input.txt', 'r')
-myList = file.readlines()
-
 validCount = 0
+validCount2 = 0
 
-for i in myList:
-    # Getting min and max values containing 1 number
-    if i[1] == '-':
-        lowMin = int(i[0])
-        if i[3] == ' ':
-            lowMax = int(i[2])
-            check = i[4]       
-            checkString = i
-            count = len(re.findall(check, checkString))
-            if count - 1 >= lowMin and count - 1 <= lowMax:
-                validCount += 1
-        else:
-            max1 = i[2]
-            max2 = i[3]
+for i in file.readlines():
+    # Splitting in 3dim by ' '
+    (numbers, needle, phrase) = (i.split(' '))
+    
+    # Extracting the min and max (left and right)
+    (left, right) = numbers.split('-')
 
-            letterMax = max1 + max2
-            endmax = int(letterMax)
-            
-            check = i[5]
-            
-            # I should do this in a function..
-            checkString = i
-            count = len(re.findall(check, checkString))
+    # Getting the char you need to count and strip the string
+    (needle, phrase) = (needle.replace(':', ''), phrase.strip())
+    
+    ## Task 2
+    if phrase[(int(left)) - 1] == needle and phrase[(int(right)) - 1] != needle:
+            validCount2 += 1
 
-            if count - 1 >= lowMin and count - 1 <= endmax:
-                validCount += 1
+    if phrase[(int(left)) - 1] != needle and phrase[(int(right)) - 1] == needle:
+        validCount2 += 1
 
-
-    # Getting min values containing 2 numbers
-    if i[1] != '-':
-        min1 = i[0]
-        min2 = i[1]
-
-        # Putting String together
-        letterMin = min1 + min2
-        endmin = int(letterMin)
-        
-
-        # MinVale is already containing 2 numbers --> max also has two numbers
-        max1 = i[3]
-        max2 = i[4]
-
-        letterMax = max1 + max2
-        endmax = int(letterMax)
-        
-        check = i[6]
-
-        checkString = i
-        count = len(re.findall(check, checkString))
-
-        if(count - 1 >= endmin and count - 1 <= endmax):
-            validCount += 1
-
+    # If valid input increase count by 1
+    if int(left) <= phrase.count(needle) <= int(right): validCount += 1
 
 print(validCount)
-print(len(myList))
+print(validCount2)
